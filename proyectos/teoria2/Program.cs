b2bf4269c9326dda04b879873282c775739449f2
Jorch95 <Jorch95@gmail.com>
2023-03-17 22:01:27 -0300

    Second commit

---
+++ proyectos/teoria2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.WriteLine("Hello, World!");
+
+String? st1 =  null; 
+String? st2 = "Hola"; 
+//Console.WriteLine(st1.GetType);
+
+int var1=0; 
+int var2 = 17;
+double var3 = 1.5;
+object obj1;
+object? obj2 = null;
+Console.WriteLine(var1);
+
+char c1 = 'A';
+char c2 = c1;
+Console.WriteLine(c1 == c2);
+object o1 = 'A';
+object o2 = o1;
+Console.WriteLine( o1 == o2);
